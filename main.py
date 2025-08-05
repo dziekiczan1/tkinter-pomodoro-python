86be912679227737c473fb91c44967c00d74480a
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-08-05 19:45:27 +0200

    add count down mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -12,9 +13,23 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(WORK_MIN * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count // 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        # Logic for when the timer reaches zero
+        pass
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -30,7 +45,7 @@ canvas.create_image(100, 112, image=tomato_image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", bg=GREEN, fg="white", font=(FONT_NAME, 12, "bold"))
+start_button = Button(text="Start", command=start_timer, bg=GREEN, fg="white", font=(FONT_NAME, 12, "bold"))
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", bg=RED, fg="white", font=(FONT_NAME, 12, "bold"))
